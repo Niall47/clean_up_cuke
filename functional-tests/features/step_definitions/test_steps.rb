1c1a98fddb233f6d1d7f2bbc867f09405efe778a
nialljames <niall.james@dvla.gov.uk>
2023-03-08 10:57:48 +0000

    adding demo test pack

---
+++ functional-tests/features/step_definitions/test_steps.rb
@@ -0,0 +1,15 @@
+Given('I have a step definition that is used') do
+  puts ' '
+end
+
+Given('I have a step definition that is not used') do
+  puts ' '
+end
+
+When('I run this scenario') do
+  puts ' '
+end
+
+Then('I should see a message that says it passed') do
+  puts ' '
+end
---
+++ functional-tests/features/test.feature
@@ -0,0 +1,5 @@
+Feature: Example feature
+
+  Scenario: Scenario 1
+    Given I have a step definition that is used
+
